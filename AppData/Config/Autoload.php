ae0932a1e8daa498cedc7bf5cc7059d8cb51fb31
iamricardo <rikirikon169@outlook.es>
2018-09-12 16:02:04 -0500

    app
    appdata

---
+++ AppData/Config/Autoload.php
@@ -0,0 +1,14 @@
+<<?php
+namespace Appdata\Config;
+
+class Autoload
+{
+  public static function run()
+  {
+    spl_autoload_register(function($class)
+    {
+      $ruta=str_replace("\\",'/',$class),'.php';
+      require_once ($ruta)
+    });
+  }
+} ?>
