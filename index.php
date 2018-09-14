bf6df5b9d475a1b743f47d8d95141a92b703dd09
iamricardo <rikirikon169@outlook.es>
2018-09-14 18:20:19 -0500

    nav
    nav_1

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php header("Content-Type: text/html;charset=utf-8");?>
-<?php session_start();
+<?php //session_start();
+
     define('DS',DIRECTORY_SEPARATOR);
     define('ROOT',realpath(dirname(__FILE__)).DS);
-    define('URL',"http://localhost/NombreDeProyecto/");
+    define('URL',"http://localhost/escuela/");
+
     require_once ("AppData/Config/Autoload.php");
     \AppData\Config\Autoload::run();
-    if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"])&&(isset($_GET['url'])?!((stristr($_GET['url'],'print'))&&(stristr($_GET['url'],'pdf'))):true))
+    if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"])&&(isset($_GET['url'])?!((stristr($_GET['url'],'print'))&&(stristr($_GET['url'],'pdf'))):true)) {
+
       Views\Template::header();
-    AppData\Config\Enrutador::run(new AppData\Config\Request());
-    if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"])&&(isset($_GET['url'])?!((stristr($_GET['url'],'print'))&&(stristr($_GET['url'],'pdf'))):true))
+      AppData\Config\Enrutador::run(new AppData\Config\Request());
+      //if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"])&&(isset($_GET['url'])?!((stristr($_GET['url'],'print'))&&(stristr($_GET['url'],'pdf'))):true))
+      Views\Template::nav();
+      Views\Template::main();
       Views\Template::footer();
+      Views\template::head();
+    }
 ?>
